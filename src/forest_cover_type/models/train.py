bcfb95fbdf704ae077195db3c293598e26b46cf6
Sabina Bayramova <sb.sabina00@gmail.com>
2022-04-27 19:02:34 +0300

    Train LogisticRegression

--- src/forest_cover_type/models/train.py
+++ src/forest_cover_type/models/train.py
@@ -1,6 +1,10 @@
 import click
 
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 
 
 @click.command()
@@ -8,4 +12,15 @@ import pandas as pd
 def train(dataset_path):
     """Script that trains a model and saves it to a file."""
     data = pd.read_csv(dataset_path, index_col="Id")
-    click.echo(f"data shape: {data.shape}")
+    X = data.drop(columns=['Cover_Type'])
+    y = data['Cover_Type']
+    X_train, X_val, y_train, y_val = train_test_split(
+        X, y, test_size=0.2, random_state=42)
+
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    classifier = LogisticRegression(max_iter=500)
+    classifier.fit(scaler.transform(X_train), y_train)
+    accuracy = accuracy_score(
+        y_true=y_val, y_pred=classifier.predict(scaler.transform(X_val)))
+    click.echo(f"accuracy = {accuracy}")
