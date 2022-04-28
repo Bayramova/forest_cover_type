4760b63146debb3a14685317a8c23773720c14ff
Sabina Bayramova <sb.sabina00@gmail.com>
2022-04-28 15:21:24 +0300

    Add use-scaler CLI option

--- src/forest_cover_type/models/make_pipeline.py
+++ src/forest_cover_type/models/make_pipeline.py
@@ -3,8 +3,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 
 
-def make_pipeline(logreg_c, max_iter):
-    scaler = StandardScaler()
+def make_pipeline(use_scaler, logreg_c, max_iter):
+    steps = []
+    if use_scaler:
+        steps.append(("scaler", StandardScaler()))
     classifier = LogisticRegression(C=logreg_c, max_iter=max_iter)
-    pipeline = Pipeline(steps=[("scaler", scaler), ("clf", classifier)])
-    return pipeline
+    steps.append(("clf", classifier))
+    return Pipeline(steps=steps)
--- src/forest_cover_type/models/train.py
+++ src/forest_cover_type/models/train.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from joblib import dump
 
 import click
@@ -15,15 +16,17 @@ from forest_cover_type.models.make_pipeline import make_pipeline
 @click.option("-s", "--save-model-path", default="models/model.joblib", show_default=True, help="Path to save trained model.")
 @click.option("--test-split-ratio", default=0.2, show_default=True, help="Proportion of the dataset to include in the test split, should be between 0.0 and 1.0.")
 @click.option("--random-state", default=42, show_default=True, help="Random state.")
+@click.option("--use-scaler", default=True, show_default=True, help="Specifies whether to scale the data.")
 @click.option("--logreg-c", default=1.0, show_default=True, help="Inverse of regularization strength.")
 @click.option("--max-iter", default=100, show_default=True, help="Maximum number of iterations taken for the solvers to converge.")
 @click.option("--k-folds", default=5, show_default=True, help="Number of folds in cross-validation.")
-def train(dataset_path, save_model_path, test_split_ratio, random_state, logreg_c, max_iter, k_folds):
+def train(dataset_path, save_model_path, test_split_ratio, random_state, use_scaler, logreg_c, max_iter, k_folds):
     """Script that trains a model and saves it to a file."""
     X_train, X_val, y_train, y_val = load_dataset(
         dataset_path=dataset_path, test_split_ratio=test_split_ratio, random_state=random_state)
 
-    pipeline = make_pipeline(logreg_c=logreg_c, max_iter=max_iter)
+    pipeline = make_pipeline(use_scaler=use_scaler,
+                             logreg_c=logreg_c, max_iter=max_iter)
     scores = cross_validate(pipeline, pd.concat([X_train, X_val]), pd.concat(
         [y_train, y_val]), cv=k_folds, scoring=('accuracy', 'neg_log_loss', 'roc_auc_ovr'))
     click.echo(
