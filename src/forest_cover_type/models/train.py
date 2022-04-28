18d6189a3ccaf12e1bb54e5ef8326c8925d26a5f
Sabina Bayramova <sb.sabina00@gmail.com>
2022-04-28 11:43:02 +0300

    Save trained model

--- src/forest_cover_type/models/train.py
+++ src/forest_cover_type/models/train.py
@@ -1,3 +1,5 @@
+from joblib import dump
+
 import click
 
 import pandas as pd
@@ -9,11 +11,12 @@ from sklearn.preprocessing import StandardScaler
 
 @click.command()
 @click.option("-d", "--dataset-path", default="data/train.csv", show_default=True, help="Path to csv with data.")
+@click.option("-s", "--save-model-path", default="models/model.joblib", show_default=True, help="Path to save trained model.")
 @click.option("--test-split-ratio", default=0.2, show_default=True, help="Proportion of the dataset to include in the test split, should be between 0.0 and 1.0.")
 @click.option("--random-state", default=42, show_default=True, help="Random state.")
 @click.option("--logreg-c", default=1.0, show_default=True, help="Inverse of regularization strength.")
 @click.option("--max-iter", default=100, show_default=True, help="Maximum number of iterations taken for the solvers to converge.")
-def train(dataset_path, test_split_ratio, random_state, logreg_c, max_iter):
+def train(dataset_path, save_model_path, test_split_ratio, random_state, logreg_c, max_iter):
     """Script that trains a model and saves it to a file."""
     data = pd.read_csv(dataset_path, index_col="Id")
     X = data.drop(columns=['Cover_Type'])
@@ -28,3 +31,5 @@ def train(dataset_path, test_split_ratio, random_state, logreg_c, max_iter):
     accuracy = accuracy_score(
         y_true=y_val, y_pred=classifier.predict(scaler.transform(X_val)))
     click.echo(f"accuracy = {accuracy}")
+    dump(classifier, save_model_path)
+    click.echo(f"Model is saved to {save_model_path}.")
