1b093c2c7d1dd43bf81ecba270b28d88042259d4
Sabina Bayramova <sb.sabina00@gmail.com>
2022-04-28 16:14:44 +0300

    Add experiments tracking to mlflow

--- src/forest_cover_type/models/train.py
+++ src/forest_cover_type/models/train.py
@@ -1,6 +1,7 @@
 from joblib import dump
 
 import click
+import mlflow
 
 import pandas as pd
 from sklearn.model_selection import cross_validate
@@ -20,20 +21,31 @@ from forest_cover_type.models.make_pipeline import make_pipeline
 @click.option("--k-folds", default=5, show_default=True, help="Number of folds in cross-validation.")
 def train(dataset_path, save_model_path, test_split_ratio, random_state, use_scaler, logreg_c, max_iter, k_folds):
     """Script that trains a model and saves it to a file."""
-    X_train, X_val, y_train, y_val = load_dataset(
-        dataset_path=dataset_path, test_split_ratio=test_split_ratio, random_state=random_state)
+    with mlflow.start_run():
+        X_train, X_val, y_train, y_val = load_dataset(
+            dataset_path=dataset_path, test_split_ratio=test_split_ratio, random_state=random_state)
 
-    pipeline = make_pipeline(use_scaler=use_scaler,
-                             logreg_c=logreg_c, max_iter=max_iter)
-    scores = cross_validate(pipeline, pd.concat([X_train, X_val]), pd.concat(
-        [y_train, y_val]), cv=k_folds, scoring=('accuracy', 'neg_log_loss', 'roc_auc_ovr'))
-    click.echo(
-        f"Mean accuracy across all CV splits: {scores['test_accuracy'].mean()}")
-    click.echo(
-        f"Mean neg_log_loss across all CV splits: {scores['test_neg_log_loss'].mean()}")
-    click.echo(
-        f"Mean roc_auc_ovr across all CV splits: {scores['test_roc_auc_ovr'].mean()}")
+        pipeline = make_pipeline(use_scaler=use_scaler,
+                                 logreg_c=logreg_c, max_iter=max_iter)
+        scores = cross_validate(pipeline, pd.concat([X_train, X_val]), pd.concat(
+            [y_train, y_val]), cv=k_folds, scoring=('accuracy', 'neg_log_loss', 'roc_auc_ovr'))
+        accuracy = scores['test_accuracy'].mean()
+        log_loss = - scores['test_neg_log_loss'].mean()
+        roc_auc = scores['test_roc_auc_ovr'].mean()
+        click.echo(
+            f"Mean accuracy across all CV splits: {accuracy}")
+        click.echo(
+            f"Mean log_loss across all CV splits: {log_loss}")
+        click.echo(
+            f"Mean roc_auc_ovr across all CV splits: {roc_auc}")
 
-    pipeline.fit(pd.concat([X_train, X_val]), pd.concat([y_train, y_val]))
-    dump(pipeline, save_model_path)
-    click.echo(f"Model is saved to {save_model_path}.")
+        pipeline.fit(pd.concat([X_train, X_val]), pd.concat([y_train, y_val]))
+        dump(pipeline, save_model_path)
+        click.echo(f"Model is saved to {save_model_path}.")
+
+        mlflow.log_param("use_scaler", use_scaler)
+        mlflow.log_param("logreg_c", logreg_c)
+        mlflow.log_param("max_iter", max_iter)
+        mlflow.log_metrics(
+            {"accuracy": accuracy, "log_loss": log_loss, "roc_auc": roc_auc})
+        mlflow.sklearn.log_model(pipeline, "model")
