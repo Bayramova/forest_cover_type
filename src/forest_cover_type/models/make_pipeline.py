0b331405b83b41801a5ca7dc8d6f708f5b283fb1
Sabina Bayramova <sb.sabina00@gmail.com>
2022-05-01 11:12:51 +0300

    Add penalty CLI option

--- src/forest_cover_type/models/make_pipeline.py
+++ src/forest_cover_type/models/make_pipeline.py
@@ -4,13 +4,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 
 
-def make_pipeline(model, use_scaler, logreg_c, max_iter, random_state, n_estimators, max_depth, min_samples_split):
+def make_pipeline(model, use_scaler, logreg_c, max_iter, penalty, random_state, n_estimators, max_depth, min_samples_split):
     steps = []
     if use_scaler:
         steps.append(("scaler", StandardScaler()))
 
     if model == "LogisticRegression":
-        classifier = LogisticRegression(C=logreg_c, max_iter=max_iter)
+        classifier = LogisticRegression(
+            C=logreg_c, max_iter=max_iter, penalty=penalty)
     elif model == "RandomForestClassifier":
         classifier = RandomForestClassifier(
             n_estimators=n_estimators, max_depth=None if max_depth == -1 else max_depth, min_samples_split=min_samples_split, random_state=random_state)
--- src/forest_cover_type/models/train.py
+++ src/forest_cover_type/models/train.py
@@ -18,18 +18,19 @@ from forest_cover_type.models.make_pipeline import make_pipeline
 @click.option("--use-scaler", default=True, show_default=True, help="Specifies whether to scale the data.")
 @click.option("--logreg-c", default=1.0, show_default=True, help="Inverse of regularization strength.")
 @click.option("--max-iter", default=100, show_default=True, help="Maximum number of iterations taken for the solvers to converge.")
+@click.option("--penalty", default="l2", show_default=True, type=click.Choice(["l2", "none"]), help="Specify the norm of the penalty.")
 @click.option("--k-folds", default=5, show_default=True, help="Number of folds in cross-validation.")
 @click.option("--model", default="LogisticRegression", type=click.Choice(["LogisticRegression", "RandomForestClassifier"]), show_default=True, help="Name of model for training.")
 @click.option("--n-estimators", default=100, show_default=True, help="The number of trees in the forest.")
 @click.option("--max-depth", default=-1, show_default=True, help="The maximum depth of the tree.")
 @click.option("--min-samples-split", default=2, show_default=True, help="The minimum number of samples required to split an internal node.")
-def train(dataset_path, save_model_path, save_best_model_path, random_state, use_scaler, logreg_c, max_iter, k_folds, model, n_estimators, max_depth, min_samples_split):
+def train(dataset_path, save_model_path, save_best_model_path, random_state, use_scaler, logreg_c, max_iter, penalty, k_folds, model, n_estimators, max_depth, min_samples_split):
     """Script that trains a model and saves it to a file."""
     with mlflow.start_run(run_name=model):
         X, y = load_dataset(dataset_path=dataset_path)
 
         pipeline = make_pipeline(model=model, use_scaler=use_scaler,
-                                 logreg_c=logreg_c, max_iter=max_iter, random_state=random_state, n_estimators=n_estimators, max_depth=max_depth, min_samples_split=min_samples_split)
+                                 logreg_c=logreg_c, max_iter=max_iter, penalty=penalty, random_state=random_state, n_estimators=n_estimators, max_depth=max_depth, min_samples_split=min_samples_split)
 
         scores = cross_validate(pipeline, X, y, cv=k_folds, scoring=(
             'accuracy', 'neg_log_loss', 'roc_auc_ovr'))
