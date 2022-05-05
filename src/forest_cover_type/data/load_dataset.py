8ff56ca3ea9b57493fd05bc8af5cfdd6456e89f0
Sabina Bayramova <sb.sabina00@gmail.com>
2022-05-05 23:02:37 +0300

    Add type annotations to load_dataset.py

--- src/forest_cover_type/data/load_dataset.py
+++ src/forest_cover_type/data/load_dataset.py
@@ -1,8 +1,11 @@
+from pathlib import Path
+from typing import Tuple
+
 import click
 import pandas as pd
 
 
-def load_dataset(dataset_path):
+def load_dataset(dataset_path: Path) -> Tuple[pd.DataFrame, pd.Series]:
     data = pd.read_csv(dataset_path, index_col="Id")
     X = data.drop(columns=["Cover_Type"])
     y = data["Cover_Type"]
