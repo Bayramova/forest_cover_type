320d79fe1218f2e55a5316ad7ae30ac8aa67b1b7
Sabina Bayramova <sb.sabina00@gmail.com>
2022-05-05 23:32:18 +0300

    Add noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,74 @@
+import os
+import tempfile
+from typing import Any
+
+import nox
+from nox.sessions import Session
+
+nox.options.sessions = "lint", "mypy", "tests"
+locations = "src", "tests", "noxfile.py"
+
+
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
+    """Install packages constrained by Poetry's lock file.
+    By default newest versions of packages are installed,
+    but we use versions from poetry.lock instead to guarantee reproducibility of sessions.
+
+    Arguments:
+        session: The Session object.
+        args: Command-line arguments for pip.
+        kwargs: Additional keyword arguments for Session.install.
+    """
+    requirements = tempfile.NamedTemporaryFile(mode="w", delete=False)
+    session.run(
+        "poetry",
+        "export",
+        "--dev",
+        "--format=requirements.txt",
+        "--without-hashes",
+        f"--output={requirements.name}",
+        external=True,
+    )
+    session.install(f"--constraint={requirements.name}", *args, **kwargs)
+    requirements.close()
+    os.remove(requirements.name)
+
+
+@nox.session(python="3.9")
+def black(session: Session) -> None:
+    """Run black code formatter."""
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
+    session.run("black", *args)
+
+
+@nox.session(python="3.9")
+def lint(session: Session) -> None:
+    """Run flake8 linting."""
+    args = session.posargs or locations
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python="3.9")
+def mypy(session: Session) -> None:
+    """Type-check using mypy."""
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
+
+
+@nox.session(python="3.9")
+def tests(session: Session) -> None:
+    """Run the test suite."""
+    args = session.posargs
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(session, "pytest")
+    session.run("pytest", "-p", "no:faulthandler", *args)
